c535213c35f4c1e20d7fab8897e8c66eb277a37c
Kathryn Stavish <kstavish@login-students.ccs.neu.edu>
2019-04-20 22:29:27 -0400

    smiling thru the pain

--- test.py
+++ test.py
@@ -88,12 +88,12 @@ trials.append(run_test('partition-2.json', 'No drops, 2 easy partitions, 20% rea
 trials.append(run_test('partition-3.json', 'No drops, 1 hard partition, 20% read'))
 trials.append(run_test('partition-4.json', 'No drops, 2 hard partitions, 20% read'))
 
-ldr = open(LEADERBOARD_OUTPUT + getpass.getuser(), 'w')
+#ldr = open(LEADERBOARD_OUTPUT + getpass.getuser(), 'w')
 
 print 'Advanced tests:'
-trials.append(run_test('advanced-1.json', '10% drops, 2 hard partitions and 1 leader failures, 20% read', ldr))
-trials.append(run_test('advanced-2.json', '15% drops, 2 leader failures, 20% read', ldr))
-trials.append(run_test('advanced-3.json', '50% drops, 1 leader failure, 20% read', ldr))
-trials.append(run_test('advanced-4.json', '10% drops, 3 hard partions and 1 leader kill, 20% read', ldr))
+trials.append(run_test('advanced-1.json', '10% drops, 2 hard partitions and 1 leader failures, 20% read'))
+trials.append(run_test('advanced-2.json', '15% drops, 2 leader failures, 20% read'))
+trials.append(run_test('advanced-3.json', '50% drops, 1 leader failure, 20% read'))
+trials.append(run_test('advanced-4.json', '10% drops, 3 hard partions and 1 leader kill, 20% read'))
 
 print 'Passed', len(filter(None, trials)), 'out of', len(trials), 'tests'
